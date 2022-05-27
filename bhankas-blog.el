552142ba385dbeb74c71aa05dd44f5bdaac67e11
Payas Relekar <relekarpayas@gmail.com>
2022-05-27 10:34:50 +0530

    bhankas-blog.el: Minor formatting

--- bhankas-blog.el
+++ bhankas-blog.el
@@ -36,19 +36,22 @@
 (defun bhankas-blog-org-publish-update-index ()
   "Update `TITLE' and `FILE-ID' in blog index."
   (interactive)
-  (with-temp-buffer (insert-file-contents "~/org/blog/src/index.org")
-                    (maphash (lambda (key value)
-                               (let ((title (car value))
-                                     (file-id (cdr value)))
-                                 (unless (search-forward file-id nil t)
-                                   (goto-char (point-max))
-                                   (write-region (format (concat  "| [[id:%s][%s]] | \\hellip |  " (format-time-string "%Y-%m-%d") " | \n")
-                                                         file-id title)
-                                                 nil
-                                                 "~/org/blog/src/index.org"
-                                                 'append)
-                                   (goto-char (point-min)))))
-                             bhankas-blog-attr-hashtable)))
+  (with-temp-buffer
+    (insert-file-contents "~/org/blog/src/index.org")
+    (maphash (lambda (key value)
+               (let ((title (car value))
+                     (file-id (cdr value)))
+                 (unless (search-forward file-id nil t)
+                   (goto-char (point-max))
+                   (write-region (format (concat  "| [[id:%s][%s]] | \\hellip |  "
+                                                  (format-time-string "%Y-%m-%d")
+                                                  " | \n")
+                                         file-id title)
+                                 nil
+                                 "~/org/blog/src/index.org"
+                                 'append)
+                   (goto-char (point-min)))))
+             bhankas-blog-attr-hashtable)))
 
 (defun bhankas-blog-rebuild ()
   "Rebuild the blog."
