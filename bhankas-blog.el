7b998c18218786b8023af8a81acfbe67ac75b9ed
Payas Relekar <relekarpayas@gmail.com>
2022-06-10 15:42:20 +0530

    Start rewrite of index-generation algorithm
    Improve post-attributes data structure to now be easily accessible property list
    mapped to title.
    
    This allows reverse-chronologically sorting the posts.
    
    Next up: actually writing the index.org file, as it now needs to be generated
    wholesale every time.

--- bhankas-blog.el
+++ bhankas-blog.el
@@ -10,6 +10,7 @@
 (defconst bhankas-blog-src-path  "~/org/blog/src/posts/")
 (defvar bhankas-blog-src-file-list)
 (defvar bhankas-blog-attr-hashtable)
+(defvar bhankas-blog-posts-list)
 
 (defun bhankas-blog-src-list-files ()
   "List all post files."
@@ -17,22 +18,44 @@
         (mapcar (lambda (fname) (concat bhankas-blog-src-path fname))
                 (remove "rss.org" (directory-files bhankas-blog-src-path nil "\.org$" t)))))
 
-(defun bhankas-blog-get-file-attrs (file-path)
-  "Get TITLE and ID of `FILE-PATH'."
-  (with-temp-buffer (insert-file-contents file-path)
-                    (cons (mapconcat 'identity (cdar (org-collect-keywords '("TITLE"))) "")
-                          (with-no-warnings
-                            (goto-line 2)
-                            (replace-regexp-in-string "\\(^:ID:[ \t]+\\|\n\\)" "" (thing-at-point 'line t))))))
+(defun bhankas-get-file-attrs (file-path)
+  "Get TITLE and ID of `FILE-PATH'.
+
+Returns a property list of:
+1. Title
+2. ID
+3. Date_created
+4. Path."
+  (with-temp-buffer
+    (insert-file-contents file-path)
+    (let ((keywords (mapcar #'car (mapcar #'cdr (org-collect-keywords '("TITLE" "DATE")))))
+          (id (with-no-warnings
+                (goto-line 2)
+                (replace-regexp-in-string "\\(^:ID:[ \t]+\\|\n\\)" "" (thing-at-point 'line t)))))
+      `(:title ,(car keywords) :id ,id :date_created ,(cadr keywords) :path ,file-path))))
 
 (defun bhankas-blog-get-src-attrs ()
-  "Generate a table with title and ID of all posts."
+  "Generate a table with TITLE as key and attribute property-list as value.
+
+Returns a hashmap where:
+key = title
+value = property-list of title, ID, date_created and path."
   (setq bhankas-blog-attr-hashtable (make-hash-table :size (length bhankas-blog-src-file-list) :test #'equal))
   (dolist (file bhankas-blog-src-file-list)
-    (puthash file (bhankas-blog-get-file-attrs file) bhankas-blog-attr-hashtable)))
+    (let (attrs)
+      (setq attrs (bhankas-get-file-attrs file))
+      (puthash (plist-get attrs :title) attrs bhankas-blog-attr-hashtable))))
 
-; done : for now.
-; TODO: Sort descending by date published (latest first).
+(defun bhankas-index-sort-reverse-chrono ()
+  "List posts reverse chronologically."
+  (setq bhankas-blog-posts-list
+        (let ((values ()))
+          (maphash (lambda (k v) (push v values)) bhankas-blog-attr-hashtable)
+          (cl-stable-sort values
+                          #'string>
+                          :key #'(lambda (attrs) (plist-get attrs :date_created))))))
+
+; TODO: Rewrite for new plist data structure
 (defun bhankas-blog-org-publish-update-index ()
   "Update `TITLE' and `FILE-ID' in blog index."
   (interactive)
